dd08724a9b65f6299cbc67170d88ed8ad72b571b
TelRoY <telroychik@gmail.com>
2025-12-28 19:17:01 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,29 @@
+// import { partial } from "lodash-es";
+import { resolve } from "path";
 import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+// import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  root: resolve(__dirname),
   build: {
-    outDir: "/dist",
+    outDir: "MyMate/dist",
     emptyOutDir: true,
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components'
-    }
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": "/src",
+  //     "@components": "src/components",
+  //   },
+  // },
   server: { port: 3000 },
   // plugins: [
   //   handlebars({
+  //     partialDirectory: resolve(__dirname, "src/layouts"),
   //     context: {
-  //       title: 'MyMate',
-  //       user: {
-  //         name: 'Anton',
-  //       }
-  //     }
-  //   })
+  //       title: "MyMate",
+  //       currentYear: new Date().getFullYear(),
+  //     },
+  //   }),
   // ],
   css: {
     postcss: "postcss.config.js",
