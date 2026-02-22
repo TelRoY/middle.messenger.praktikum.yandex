86741bd563919f8100631bc3a4417407e4b1d7b4
TelRoY <telroychik@gmail.com>
2026-02-22 12:33:52 +0300

    del commits

--- src/api/ChatsAPI.ts
+++ src/api/ChatsAPI.ts
@@ -1,12 +1,9 @@
 import { api } from '../utils/apiRequest';
-// import { checkResponse, ErrorResponse } from '../utils/checkResponse';
 import { Chat, ChatDTO, ChatMessage, MessageDTO, AddUserToChatData, DeleteUserFromChatData, CreateChatData } from '../models/Chat';
 import { UserDTO } from '../models/User';
 
 export class ChatsAPI {
   static async getChats(): Promise<Chat[]> {
-    // const response = await apiClient.get<ChatDTO[] | ErrorResponse>('/chats');   
-    // const chats = response.data as ChatDTO[];
     const chats = await api.get<ChatDTO[]>('/chats');
 
     return chats.map(chat => ({
@@ -21,42 +18,18 @@ export class ChatsAPI {
 
   static async createChat(data: CreateChatData): Promise<{ id: number }> {
     return api.post<{ id: number }>('/chats', data);
-    // const requestData = data as unknown as Record<string, unknown>;
-    // const response = await apiClient.post<{ id: number } | ErrorResponse>(
-    //   '/chats',
-    //   requestData
-    // );
-    
-    // return checkResponse<{ id: number }>(response);
   }
 
   static async deleteChat(chatId: number): Promise<void> {
     await api.delete<void>('/chats', { chatId });
-    // const response = await apiClient.delete<ErrorResponse>(
-    //   '/chats',
-    //   { data: { chatId } as Record<string, unknown> }
-    // );
-
-    // checkResponse<void>(response);
   }
 
   static async getToken(chatId: number): Promise<string> {
     const response = await api.post<{ token: string }>(`/chats/token/${chatId}`, {});
     return response.token;
-    // const response = await apiClient.post<{ token: string } | ErrorResponse>(
-    //   `/chats/token/${chatId}`,
-    //   {}
-    // );
-    // const data = checkResponse<{ token: string }>(response);
-
-    // return data.token;
   }
 
   static async getChatMessages(chatId: number): Promise<ChatMessage[]> {
-    // const response = await apiClient.get<MessageDTO[] | ErrorResponse>(
-    //   `/chats/${chatId}/messages`
-    // );
-    // const messages = checkResponse<MessageDTO[]>(response);
     const messages = await api.get<MessageDTO[]>(`/chats/${chatId}/messages`);
 
     return messages.map(msg => ({
@@ -73,33 +46,14 @@ export class ChatsAPI {
 
   static async addUserToChat(data: AddUserToChatData): Promise<void> {
     await api.put<void>('/chats/users', data);
-    // const requestData = data as unknown as Record<string, unknown>;
-    // const response = await apiClient.put<ErrorResponse>(
-    //   '/chats/users',
-    //   requestData
-    // );
-    
-    // checkResponse<void>(response);
   }
 
   static async deleteUserFromChat(data: DeleteUserFromChatData): Promise<void> {
     await api.delete<void>('/chats/users', data);
-    // const requestData = data as unknown as Record<string, unknown>;
-    // const response = await apiClient.delete<ErrorResponse>(
-    //   '/chats/users',
-    //   { data: requestData }
-    // );
-
-    // checkResponse<void>(response);
   }
 
   static async getChatUsers(chatId: number): Promise<UserDTO[]> {
     return api.get<UserDTO[]>(`/chats/${chatId}/users`);
-    // const response = await apiClient.get<UserDTO[] | ErrorResponse>(
-    //   `/chats/${chatId}/users`
-    // );
-    
-    // return checkResponse<UserDTO[]>(response);
   }
 
   static async uploadChatAvatar(chatId: number, file: File): Promise<Chat> {
@@ -108,12 +62,6 @@ export class ChatsAPI {
     formData.append('chatId', chatId.toString());
 
     const chat = await api.put<ChatDTO>('/chats/avatar', formData);
-    // const response = await apiClient.put<ChatDTO | ErrorResponse>(
-    //   '/chats/avatar',
-    //   formData
-    // );
-    
-    // const chat = checkResponse<ChatDTO>(response);
     return {
       id: chat.id,
       title: chat.title,
